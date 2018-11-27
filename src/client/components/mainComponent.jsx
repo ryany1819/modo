d66a677969e17a5be500f231dd89625a7772bc9b
sratemo <sratemo@gmail.com>
2018-11-27 11:35:57 -0800

    client/server folders under src

---
+++ src/client/components/mainComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Group from './group'
+
+
+const mainComponent = () => (
+
+  <div>
+   <h> Main </h>
+  </div>
+);
+
+export default mainComponent;
---
+++ src/client/reducers/userReducer.js
@@ -0,0 +1,20 @@
+const initialState = {
+  isLoggedIn: false,
+  loggedInUser: '',
+  signUpInputUsername: '',
+  signUpInputPassword: '',
+  signUpInputEmail: '',
+  loginInputEmail: '',
+  loginInputPassword: '',
+  signUpError: false,
+  signUpErrorMsg: '',
+  loginError: false,
+  loginErrorMsg: '',
+  passwordErrorMsg: '',
+  usernameErrorMsg: '',
+  emailErrorMsg: '',
+  forgotInputEmail: '',
+  emailStatusMsg: '',
+  emailSuccess: false,
+};
+
