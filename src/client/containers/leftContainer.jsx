dca95b0ac5593648119421efb5ca7ed0c9512bd2
sratemo <sratemo@gmail.com>
2018-11-27 20:56:18 -0800

    new react components

---
+++ src/client/containers/leftContainer.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable import/extensions */
+import React from 'react';
+
+import Group from '../components/group.jsx';
+
+const leftContainer = () => (
+
+  <div className="leftpanel">
+
+    <Group />
+    <Group />
+    <Group />
+    <Group />
+  </div>
+);
+
+export default leftContainer;
